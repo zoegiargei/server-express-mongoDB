3ca1564c765c700d7279a8c8af97d104a95a9480
Zoe Giargei <zoegiargei@MacBook-Air-de-Javier-2.local>
2023-04-03 09:02:16 -0300

    Add prevLink & nextLink success

--- src/controllers/controllersWeb.js
+++ src/controllers/controllersWeb.js
@@ -15,11 +15,20 @@ export const contrShowProdByPaginate = async (req, res) => {
     const allProducts = await productsService.productsByPaginate(1, page)
 
     const thIsProducts = allProducts['docs'].length > 0
-    const prevLink = allProducts.hasPrevPage ? Number(page)-1 : null
-    const nextLink = allProducts.hasPrevPage ? Number(page)+1 : null
+    console.log(allProducts.docs)
+    console.log(allProducts)
+    console.log(allProducts.page)
+    console.log(allProducts.hasPrevPage)
+    console.log(allProducts.hasNextPage)
+    console.log(allProducts.nextPage)
 
-    console.log(prevLink)
-    console.log(nextLink)
+    //allProducts.docs.forEach(doc => {})
 
-    res.render('products', { title: 'Products By Paginate', thIsProducts: thIsProducts, products: allProducts['docs'], prevLink: prevLink? prevLink : 'No hay pagina previa', nextLink: nextLink? nextLink : 'No hay pagina siguiente' })
+    //console.log(prevLink)
+    //console.log(nextLink)
+    const prevLink = allProducts.hasPrevPage? `http://localhost:8080/web/products?page=${allProducts.prevPage}` : null
+    const nextLink = allProducts.hasNextPage? `http://localhost:8080/web/products?page=${allProducts.nextPage}` : null
+    const numPage = allProducts.page
+
+    res.render('products', { title: 'Products By Paginate', thIsProducts: thIsProducts, products: allProducts['docs'], prevLink: prevLink? prevLink : 'No hay pagina previa', nextLink: nextLink? nextLink : 'No hay pagina siguiente' , numberPage: numPage})
 }
